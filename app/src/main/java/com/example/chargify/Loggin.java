2c861a58772bd023f261b2b891bda3450e64283a
Vrushika24 <shahvrushika2@gmail.com>
2022-11-12 17:19:49 +0530

    new progress

---
+++ app/src/main/java/com/example/chargify/Loggin.java
@@ -0,0 +1,14 @@
+package com.example.chargify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Loggin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loggin);
+    }
+}
